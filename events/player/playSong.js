84cb03f456065e9dd024be0e0aa48be7ac039105
RyoKr <115417859+ryokr@users.noreply.github.com>
2024-04-20 20:52:59 +0700

    Update playSong.js

--- events/player/playSong.js
+++ events/player/playSong.js
@@ -11,10 +11,10 @@ module.exports = async (client, queue, song) => {
           iconURL: 'https://cdn.discordapp.com/attachments/1140841446228897932/1144671132948103208/giphy.gif',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-        .setDescription(`\n ‎ \n▶️ **Details :** **${song?.name}**\n▶️ **Enjoy the Ultimate Music Experience. ** \n▶️ **If link breaks playback try to give query.**`)
+        .setDescription(`\n ‎ \n▶️ **Details :** **${song?.name}**`)
         .setImage(queue.songs[0].thumbnail)
         .setColor('#FF0000')
-        .setFooter({ text: 'More info - Use /help command [GlaceYT]' });
+        .setFooter({ text: 'More info - Use /help command' });
 
       queue?.textChannel?.send({ embeds: [embed] }).catch(e => { });
     }

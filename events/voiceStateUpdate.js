3adb13a2a9d32115953b6032af17c18b906b7ea0
RyoKr <115417859+ryokr@users.noreply.github.com>
2024-04-20 22:11:27 +0700

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -3,22 +3,24 @@ const db = require("../mongoDB");
 module.exports = async (client, oldState, newState) => {
    const queue = client.player.getQueue(oldState.guild.id);
    if (queue || queue?.playing) {
-      // if (client?.config?.opt?.voiceConfig?.leaveOnEmpty?.status === true) {
-      //    setTimeout(async () => {
-      //       let botChannel = oldState?.guild?.channels?.cache?.get(queue?.voice?.connection?.joinConfig?.channelId)
-      //       if (botChannel) {
-      //          if (botChannel.id == oldState.channelId)
-      //             if (botChannel?.members?.find(x => x == client?.user?.id)) {
-      //                if (botChannel?.members?.size == 1) {
-      //                   await queue?.textChannel?.send({ content: `🔴 Users left channel!!` }).catch(e => { })
-      //                   if (queue || queue?.playing) {
-      //                      return queue?.stop(oldState.guild.id)
-      //                   }
-      //                }
-      //             }
-      //       }
-      //    }, client?.config?.opt?.voiceConfig?.leaveOnEmpty?.cooldown || 0)
-      // }
+      if (client?.config?.opt?.voiceConfig?.leaveOnEmpty?.status === true) {
+         // setTimeout(async () => {
+         //    let botChannel = oldState?.guild?.channels?.cache?.get(queue?.voice?.connection?.joinConfig?.channelId)
+         //    if (botChannel) {
+         //       if (botChannel.id == oldState.channelId)
+         //          if (botChannel?.members?.find(x => x == client?.user?.id)) {
+         //             if (botChannel?.members?.size == 1) {
+         //                await queue?.textChannel?.send({ content: `🔴 Users left channel!!` }).catch(e => { })
+         //                if (queue || queue?.playing) {
+         //                   return queue?.stop(oldState.guild.id)
+         //                }
+         //             }
+         //          }
+         //    }
+         // }, client?.config?.opt?.voiceConfig?.leaveOnEmpty?.cooldown || 0)
+
+         setInterval(async () {}, 100000);
+      }
 
       if (newState.id === client.user.id) {
          if (oldState.serverMute === false && newState.serverMute === true) {

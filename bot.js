22f359ad853ca97bd0930edee8b51b66edba9808
RyoKr <115417859+ryokr@users.noreply.github.com>
2024-04-20 16:40:49 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -105,7 +105,7 @@ app.get('/', (req, res) => {
   res.sendFile(imagePath);
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to GlaceYT: http://localhost:${port}`);
+  console.log(`🔗 Listening to RyoKr: http://localhost:${port}`);
 });
 printWatermark();
 

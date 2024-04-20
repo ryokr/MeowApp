26b2dceb5d0c0451237fbb25e1d35a04afbdfd81
RyoKr <115417859+ryokr@users.noreply.github.com>
2024-04-20 21:29:11 +0700

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,47 +1,47 @@
 const db = require("../mongoDB");
+
 module.exports = async (client, oldState, newState) => {
-  const queue = client.player.getQueue(oldState.guild.id);
-  if (queue || queue?.playing) {
-    if (client?.config?.opt?.voiceConfig?.leaveOnEmpty?.status === true) {
-
-      // setTimeout(async () => {
-      //   let botChannel = oldState?.guild?.channels?.cache?.get(queue?.voice?.connection?.joinConfig?.channelId)
-      //   if (botChannel) {
-      //       if (botChannel.id == oldState.channelId)
-      //           if (botChannel?.members?.find(x => x == client?.user?.id)) {
-      //               if (botChannel?.members?.size == 1) {
-      //                   await queue?.textChannel?.send({ content: `🔴 Users left channel!!` }).catch(e => {})
+   const queue = client.player.getQueue(oldState.guild.id);
+   if (queue || queue?.playing) {
+      // if (client?.config?.opt?.voiceConfig?.leaveOnEmpty?.status === true) {
+      //    setTimeout(async () => {
+      //       let botChannel = oldState?.guild?.channels?.cache?.get(queue?.voice?.connection?.joinConfig?.channelId)
+      //       if (botChannel) {
+      //          if (botChannel.id == oldState.channelId)
+      //             if (botChannel?.members?.find(x => x == client?.user?.id)) {
+      //                if (botChannel?.members?.size == 1) {
+      //                   await queue?.textChannel?.send({ content: `🔴 Users left channel!!` }).catch(e => { })
       //                   if (queue || queue?.playing) {
-      //                       return queue?.stop(oldState.guild.id)
+      //                      return queue?.stop(oldState.guild.id)
       //                   }
-      //               }
-      //           }
+      //                }
+      //             }
       //       }
-      //   }, client?.config?.opt?.voiceConfig?.leaveOnEmpty?.cooldown || 0)
-    }
+      //    }, client?.config?.opt?.voiceConfig?.leaveOnEmpty?.cooldown || 0)
+      // }
 
-    if (newState.id === client.user.id) {
+      if (newState.id === client.user.id) {
+         if (oldState.serverMute === false && newState.serverMute === true) {
+            if (queue?.textChannel) {
+               try {
+                  await queue?.pause()
+               } catch (e) {
+                  return
+               }
+               await queue?.textChannel?.send({ content: `🔴 Muted` }).catch(e => { })
+            }
+         }
 
-
-      if (oldState.serverMute === false && newState.serverMute === true) {
-        if (queue?.textChannel) {
-          try {
-            await queue?.pause()
-          } catch (e) {
-            return
-          }
-          await queue?.textChannel?.send({ content: `🔴 Muted` }).catch(e => { })
-        }
+         if (oldState.serverMute === true && newState.serverMute === false) {
+            if (queue?.textChannel) {
+               try {
+                  await queue.resume();
+               } catch (e) {
+                  return
+               }
+            }
+         }
+         
       }
-      if (oldState.serverMute === true && newState.serverMute === false) {
-        if (queue?.textChannel) {
-          try {
-            await queue.resume();
-          } catch (e) {
-            return
-          }
-        }
-      }
-    }
-  }
+   }
 }

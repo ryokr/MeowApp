0cc87d2552e2c8875a408b01289d01d78d309cea
RyoKr <115417859+ryokr@users.noreply.github.com>
2024-05-04 02:23:33 +0700

    Update addSong.js

--- events/player/addSong.js
+++ events/player/addSong.js
@@ -7,12 +7,12 @@ module.exports = async (client, queue, song) => {
       if (queue?.textChannel) {
          const embed = new EmbedBuilder()
             .setAuthor({
-               name: '**Added To Queue**',
+               name: 'Added To Queue',
                iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157218651179597884/1213-verified.gif?ex=6517cf5a&is=65167dda&hm=bc8fb4414cb412587ade0af285b77569d2568214cf7d6baab8702ddeb6c38ad5&',
                url: 'https://discord.gg/fTuGFk9ayG'
             })
             .setDescription(`**${song.name}** Requested by <@${song.user.id}>`)
-            .setColor('#14bdff')
+            .setColor(client.config.embedColor)
             .setFooter({ text: 'Meow' });
          queue?.textChannel?.send({ embeds: [embed] }).catch(e => { });
       }

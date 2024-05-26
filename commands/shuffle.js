89852db831f7fc5a4544849417cd7ee441edf20b
ryokr <ryohuy2410@gmail.com>
2024-05-26 13:24:38 +0700

    Meow

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -7,7 +7,7 @@ module.exports = {
    run: async (client, interaction) => {
       try {
          const queue = client.player.getQueue(interaction.guild.id)
-         if (!queue || !queue.playing || !track)
+         if (!queue || !queue.playing)
             return interaction.reply({ content: `No music playing`, ephemeral: true }).catch((e) => {})
 
          try {

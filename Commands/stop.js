251893c36647e17709a1a7fc0e5c3c0491ab62fc
ryokr <ryohuy2410@gmail.com>
2024-06-08 04:00:11 +0700

    Meow

---
+++ Commands/stop.js
@@ -0,0 +1,28 @@
+const { EmbedBuilder } = require('discord.js')
+const { deleteMessage } = require('../Function')
+
+module.exports = {
+   name: 'stop',
+   description: 'Stop the music and clear the queue',
+   permissions: '0x0000000000000800',
+   voiceChannel: true,
+   
+   run: async (client, interaction) => {
+      try {
+         const queue = client.player.getQueue(interaction.guild.id)
+         const embed = new EmbedBuilder().setColor(client.config.player.embedColor)
+
+         if (!queue || !queue.playing) {
+            embed.setDescription('No music playing')
+         } else {
+            await queue.stop()
+            embed.setDescription('Stopped the music and cleared the queue')
+         }
+
+         const msg = await interaction.reply({ embeds: [embed] })
+         deleteMessage(msg, 10000)
+      } catch {
+         console.log('❌    Stop Error')
+      }
+   }
+}

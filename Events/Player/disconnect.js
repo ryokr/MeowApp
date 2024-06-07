c79eb30eb496bb34f28240d6e77b6c4ab4483a7a
ryokr <ryohuy2410@gmail.com>
2024-06-07 12:56:37 +0700

    Meow

--- Events/Player/disconnect.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (client, queue) => {
-   try {
-      if (queue.lastPlayingMessage != null) {
-         await queue.lastPlayingMessage.delete().catch(() => {})
-      }
-   } catch {
-      console.log('❌    No Message')
-   }
-}

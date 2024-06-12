2bc8c11958bc38ddb108c8a2e5170463e50d8f93
ryokr <ryohuy2410@gmail.com>
2024-06-12 15:43:22 +0700

    Meow

--- Commands/grab.js
+++ Commands/grab.js
@@ -11,25 +11,27 @@ module.exports = {
          const queue = client.player.getQueue(interaction.guild.id)
          const embed = new EmbedBuilder().setColor(client.config.player.embedColor)
          const loopMode = ['Off', 'Track', 'Queue']
-         
+
          if (!queue || !queue.playing) {
             embed.setDescription('No music playing')
          } else {
             const song = queue.songs[0]
             embed
                .setThumbnail(song.thumbnail)
-               .setAuthor({ name: 'Now Playing', iconURL: interaction.guild.iconURL() })
+               .setAuthor({ name: '─────・ I N F O R M A T I O N 💖・─────', iconURL: interaction.guild.iconURL() })
                .setDescription(`**[${song.name}](${song.url})**`)
                .addFields(
                   { name: 'Duration', value: `${formatTime(song.formattedDuration)}`, inline: true },
                   { name: 'Current time', value: `${formatTime(queue.formattedCurrentTime)}`, inline: true },
                   { name: 'Author', value: `${song.uploader.name}`, inline: true },
                   { name: 'Volume', value: `${queue.volume}`, inline: true },
-                  { name: 'Loop Mode', value: `${loopMode[queue.repeatMode]}`, inline: true,},
-                  { name: 'Requested by', value: `<@${song.user.id}>`, inline: true,},
+                  { name: 'Loop Mode', value: `${loopMode[queue.repeatMode]}`, inline: true },
+                  { name: 'Requested by', value: `<@${song.user.id}>`, inline: true }
                )
                .setFooter({ text: `🌱 • ${capFirstChar(song.user.tag)}`, iconURL: song.user.avatarURL() })
                .setTimestamp()
+
+            await interaction.reply({ embeds: [embed] })
          }
       } catch {}
    }

a2864e621d8a0909f48896970eab7e8308fd38e8
ryokr <ryohuy2410@gmail.com>
2024-06-20 19:10:14 +0700

    Meow

--- push.ps1
+++ push.ps1
@@ -1,41 +1,11 @@
-$sourceDir = (Get-Location).Path
-$parentDir = (Get-Item $sourceDir).Parent.FullName
-$destDir = Join-Path $parentDir "MeowBot"
-$excludeList = @("node_modules", "package-lock.json", ".env")
+$Source = Get-Location
+$Destination = Join-Path $Source "..\MeowBot"
 
-function ShouldExclude {
-   param (
-      [string]$path,
-      [string[]]$exclusions
-   )
+Get-ChildItem -Path $Destination -Exclude ".git" | Remove-Item -Recurse -Force
+robocopy $Source $Destination /E /XD "$Source\node_modules" /XF "package-lock.json" ".env"
 
-   foreach ($exclude in $exclusions) {
-      if ($path -like "*\$exclude*") {
-         return $true
-      }
-   }
-   return $false
-}
-
-Get-ChildItem -Path $destDir -Recurse -Force | Remove-Item -Recurse -Force
-
-Get-ChildItem -Path $sourceDir -Recurse -Force | ForEach-Object {
-   $relativePath = $_.FullName -replace [regex]::Escape($sourceDir), ""
-   $destPath = Join-Path $destDir $relativePath
-
-   if (-not (ShouldExclude -path $_.FullName -exclusions $excludeList)) {
-      if ($_.PSIsContainer) {
-         New-Item -Path $destPath -ItemType Directory -Force
-      } else {
-         Copy-Item -Path $_.FullName -Destination $destPath -Force
-      }
-   }
-}
-
-Set-Location $destDir
-
+Set-Location -Path $Destination
 git add --all
 git commit -S -m 'Meow'
 git push -u origin main
-
-Set-Location $sourceDir
+Set-Location -Path $Source

29c75ada53fff986f7c01af0c5559dc010861b06
ryokr <ryohuy2410@gmail.com>
2024-06-20 19:19:03 +0700

    Meow

--- push.ps1
+++ push.ps1
@@ -1,5 +1,12 @@
 $Source = Get-Location
 $Destination = Join-Path $Source "..\MeowBot"
+$ParentDir = Get-Item -Path ".."
+
+$RepoURL = "https://github.com/ryokr/MeowBot.git"
+
+Set-Location -Path $ParentDir
+git clone $RepoURL
+Set-Location -Path $Source
 
 Get-ChildItem -Path $Destination -Exclude ".git" | Remove-Item -Recurse -Force
 robocopy $Source $Destination /E /XD "$Source\node_modules" /XF "package-lock.json" ".env"

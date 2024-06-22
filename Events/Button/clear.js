95f3d9a782d632d650ca70d2db122ff88fc1da8c
ryokr <ryohuy2410@gmail.com>
2024-06-22 18:42:37 +0700

    Meow

--- Events/Button/clear.js
+++ Events/Button/clear.js
@@ -1,8 +1,8 @@
 module.exports = async (queue, embed, username, avatar) => {
    if (queue.songs.length > 1) {
       queue.songs = []
-      embed.setFooter({ text: `💽 • Queue cleared • ${username}`, iconURL: avatar })
+      embed.setFooter({ text: `💽 • Queue Cleared • ${username}`, iconURL: avatar })
    } else {
-      embed.setFooter({ text: `💽 • Queue empty • ${username}`, iconURL: avatar })
+      embed.setFooter({ text: `💽 • Queue Empty • ${username}`, iconURL: avatar })
    }
 }
--- Events/Button/grab.js
+++ Events/Button/grab.js
@@ -1,12 +1,12 @@
 const { EmbedBuilder } = require('discord.js')
 const { formatTime } = require('../../Function')
 
-module.exports = async (client, queue, song, embed, username, avatar, duration) => {
+module.exports = async (client, queue, song, embed, username, avatar) => {
    const grabEmbed = new EmbedBuilder()
       .setColor(client.config.player.embedColor)
       .setImage(song.thumbnail)
       .setAuthor({ name: '─────・ I N F O R M A T I O N 💖・─────', iconURL: queue.textChannel.guild.iconURL() })
-      .setDescription(`**[${song.name}](${song.url})**\n${song.uploader.name}・${duration}・Current ${formatTime(queue.formattedCurrentTime)}`)
+      .setDescription(`**[${song.name}](${song.url})**\n${song.uploader.name}・${queue.formattedCurrentTime} / ${song.formattedDuration}`)
       // .addFields(
       //    { name: 'Author', value: `${song.uploader.name}`, inline: true },
       //    { name: 'Duration', value: `${duration}`, inline: true },
@@ -16,7 +16,7 @@ module.exports = async (client, queue, song, embed, username, avatar, duration) 
       .setTimestamp()
 
    await queue.textChannel.send({ embeds: [grabEmbed] })
-   embed.setFooter({ text: `💽 • Info revealed • ${username}`, iconURL: avatar })
+   embed.setFooter({ text: `💽 • Song Revealed • ${username}`, iconURL: avatar })
 
    const channel = client.channels.cache.get('1235972414287118397');
    if (!channel) {

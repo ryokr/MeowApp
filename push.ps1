40dc0626fd2333674c71c672b367b7a992ee7dbc
ryokr <ryohuy2410@gmail.com>
2024-07-04 02:08:19 +0700

    From Ryo.o With ❤️‍🔥

--- push.ps1
+++ push.ps1
@@ -1,39 +1,37 @@
 $RepoURLs = @(
-   "https://github.com/ryokr/MeowBot.git",
-   "https://github.com/Pooba-Saga/MeowBot1.git",
-   "https://github.com/Pooba-Saga/MeowBot2.git"
+   "https://github.com/Pooba-Saga/MeowBot-Ei",
+   "https://github.com/Pooba-Saga/MeowBot-Yae",
+   "https://github.com/Pooba-Saga/MeowBot-Clorinde",
+   "https://github.com/Pooba-Saga/MeowBot-Roxy",
+   "https://github.com/Pooba-Saga/MeowBot-Robin",
+   "https://github.com/ryokr/MeowBot"
 )
 
-if ($args.Count -eq 0) {
-   Write-Host "No argument provided"
-   exit
-}
-
-$arg = [int]$args[0]
-if ($arg -lt 0 -or $arg -ge $RepoURLs.Count) {
-   Write-Host "Invalid argument. Please provide a valid numeric argument (0, 1, or 2)."
-   exit
-}
-
-$SelectedRepoURL = $RepoURLs[$arg]
-$RepoName = ($SelectedRepoURL -split "/")[-1] -replace "\.git$", ""
-
 $Local = Get-Location
-$Repo = Join-Path $Local "..\$RepoName"
 $Parent = Get-Item -Path ".."
 
-Set-Location -Path $Parent
-git clone $SelectedRepoURL
-Set-Location -Path $Local
+foreach ($RepoURL in $RepoURLs) {
+   $RepoName = ($RepoURL -split "/")[-1]
+   $Repo = Join-Path $Local "..\$RepoName"
 
-Get-ChildItem -Path $Repo -Exclude ".git" | Remove-Item -Recurse -Force
-robocopy $Local $Repo /E /XD "$Local\node_modules" /XF "package-lock.json" ".env"
+   Set-Location -Path $Parent
+   git clone $RepoURL
+   Set-Location -Path $Local
 
-Set-Location -Path $Repo
-git add .
-git commit -S -m 'From Ryo.o With ❤️‍🔥'
-git push origin
-Set-Location -Path $Local
+   Get-ChildItem -Path $Repo -Exclude ".git" | Remove-Item -Recurse -Force
+   if ($RepoName -ne "MeowBot") {
+      robocopy $Local $Repo /E /XD "$Local\node_modules" /XF "package-lock.json" ".env" "push.ps1"
+   } else {
+      robocopy $Local $Repo /E /XD "$Local\node_modules" /XF "package-lock.json" ".env"
+   }
 
-Remove-Item -Path $Repo -Recurse -Force
+   Set-Location -Path $Repo
+   git add .
+   git commit -S -m 'From Ryo.o With ❤️‍🔥'
+   git push origin
+   Set-Location -Path $Local
+
+   Remove-Item -Path $Repo -Recurse -Force
+}
+
 clear
